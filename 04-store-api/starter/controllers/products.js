703b04ba18cf6c3c31c2d8e3a80e55021b70ed55
Sagar Bakshi <sagarbakshi@outlook.com>
2022-07-04 02:18:40 +0530

    added basic dynamic query

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -5,8 +5,14 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find();
-  res.status(200).json({ products: products });
+  const { featured } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  const products = await Product.find(queryObject);
+
+  res.status(200).json({ products, nbhits: products.length });
 };
 
 module.exports = {
