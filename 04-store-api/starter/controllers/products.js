cb399a39c726bbb31ddb082eea05fca0983cf8a3
sagar bakshi <sagarbakshi@outlook.com>
2022-07-06 02:15:49 +0530

    added sort functionality

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,18 +1,41 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: "products route" });
+  const search = "ab";
+
+  const products = await Product.find({
+    name: { $regex: search, $options: "i"}
+  });
+
+  res.status(200).json({ products, nbhits: products.length });
 };
 
+//getting all product
 const getAllProducts = async (req, res) => {
-  const { featured } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
   }
-  const products = await Product.find(queryObject);
+  if (company){
+    queryObject.company = company;
+  }
+  if (name){
+    queryObject.name = { $regex: name, $options: "i"}
+  }
 
-  res.status(200).json({ products, nbhits: products.length });
+  let result =  Product.find(queryObject);
+
+  if (sort){
+    const sortList = sort.split(',').join(' ');
+    console.log(sortList);
+    const products = await result.sort(sortList);
+    res.status(200).json({ nbhits: products.length, products  });
+  }else {
+      const products = await result;
+      res.status(200).json({ nbhits: products.length, products  });
+  }
+
 };
 
 module.exports = {
